e9f9be3dbedd323e21aab8c4955fa1d8c79c0e18
Ioane Lomidze <ioanelomidze7@gmail.com>
2018-09-05 14:59:40 +0400

    adding passport for users, login, register and validation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://Iokinava:ahsokaTano1@ds245132.mlab.com:45132/devconnector"
+    "mongodb://Iokinava:ahsokaTano1@ds245132.mlab.com:45132/devconnector",
+  secretOrKey: "Amidala"
 };
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profiles");
 const posts = require("./routes/api/posts");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//Body Pareser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require("./config/keys").mongoURI;
@@ -15,8 +22,11 @@ mongoose
   .connect(db)
   .then(() => console.log("...Connected to mongoDB"))
   .catch(err => console.log(err));
+//Passport Middlware
+app.use(passport.initialize());
 
-app.get("/", (req, res) => res.send("გამარჯობა"));
+//Passport Config
+require("./config/passport")(passport);
 
 //Use Routes
 app.use("/api/users", users);
