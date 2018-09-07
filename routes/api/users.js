a23551f626e5a3bbc550509846b565839ba0c1fd
Ioane Lomidze <ioanelomidze7@gmail.com>
2018-09-08 00:04:44 +0400

    client side, add react components, create register, login, landing page UI

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,7 +18,7 @@ const validateLoginInput = require('../../Validation/login');
 //@access  Public
 router.get('/test', (req, res) => res.json({ message: 'Users Work' }));
 
-//@route   GET api/users/register
+//@route   POST api/users/register
 //@desc    Register user
 //@access  Public
 router.post('/register', (req, res) => {
@@ -61,7 +61,7 @@ router.post('/register', (req, res) => {
   });
 });
 
-//@route   GET api/users/login
+//@route   POST api/users/login
 //@desc    Login user / Returning JWT token
 //@access  Public
 
